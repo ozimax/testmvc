b4c57fe5f981d80fd1f375f4b3cccb93fe195eaf
my-ozzy-lab <ozanonder06@outlook.com>
2025-05-27 16:02:49 +0100

    Create blog.cs

---
+++ blog.cs
@@ -0,0 +1,13 @@
+
+namespace DemoAzureWebsite.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; } // Primary key
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; } // Foreign key to the Blogs table
+
+        public virtual Blog Blog { get; set; } // Navigation property to Blog
+    }
+}
