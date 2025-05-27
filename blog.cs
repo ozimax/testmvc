46193424bc7b4bb96180b35986e895abffc10c23
OnderO <OnderO@LFLON44203>
2025-05-27 17:06:24 +0100

    add new feature

--- blog.cs
+++ blog.cs
@@ -8,6 +8,11 @@ namespace DemoAzureWebsite.Models
         public string Content { get; set; }
         public int BlogId { get; set; } // Foreign key to the Blogs table
 
+
+        public string vum { get; set; }
+        public string kum { get; set; }
+        public int bumbum { get; set; } // Foreign key to the Blogs table
+
         public virtual Blog Blog { get; set; } // Navigation property to Blog
     }
 }
